8beb8ef1c2df1cf6cc8dcc73461304fa1c01a77b
TuNombreDeUsuario <tuemail@example.com>
2025-05-26 18:23:29 -0600

    Barra Arreglada

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Referencias
+const barra = document.getElementById('barraLateral');
+const toggle = document.querySelector('.boton-toggle');
+
+// Al hacer clic en el toggle, expandir/colapsar la barra lateral
+toggle.addEventListener('click', () => {
+  barra.classList.toggle('expandida');
+});
+
+
 import {
   fetchMiradorData,
   fetchRomeroDHT11,
